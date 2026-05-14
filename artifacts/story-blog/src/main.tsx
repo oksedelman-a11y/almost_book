86d6dbd9dbc9d7490ac96fb4d4bb4c73246bcf95
Oksedelman <55379142-oksedelman@users.noreply.replit.com>
2026-05-14 13:05:56 +0000

    add api base url

--- artifacts/story-blog/src/main.tsx
+++ artifacts/story-blog/src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { setAuthTokenGetter } from "@workspace/api-client-react";
+import { setAuthTokenGetter, setBaseUrl } from "@workspace/api-client-react";
 import App from "./App";
 import "./index.css";
+
+// Указываем, где находится наш API-сервер
+setBaseUrl("https://almost-book-api-server.vercel.app");
 
 setAuthTokenGetter(() => {
   try {
