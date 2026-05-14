044106fe5624613b58ba068d427c726b2ce85c2c
Oksedelman <55379142-oksedelman@users.noreply.replit.com>
2026-05-14 15:39:23 +0000

    fix:stable vite vercel build

--- artifacts/story-blog/vite.config.ts
+++ artifacts/story-blog/vite.config.ts
@@ -1,17 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+
   base: "/",
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      "@": "/src"
     }
   },
+
   build: {
     outDir: "dist/public",
-    emptyOutDir: true
+    emptyOutDir: true,
+    assetsDir: "assets",
+    sourcemap: false
   }
 });
